b504523b90a8df7230b32c6a90b2c6d7531677a4
nishida675 <139204422+nishida675@users.noreply.github.com>
2023-11-19 20:40:09 +0900

    Initial commit

---
+++ src/components/pages/Users.jsx
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
+import { useContext } from "react";
+import { UserContext } from "../../providers/UserProvider";
+import { useRecoilState } from "recoil";
+import { userState } from "../../store/userState";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `たかし-${val}`,
+    image:
+      "https://images.unsplash.com/photo-1583512603805-3cc6b41f3edb?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    email: "12345@example.com",
+    phone: "090-1111-22222",
+    company: {
+      name: "テスト会社"
+    },
+    website: "https://google.com"
+  };
+});
+
+export const Users = () => {
+  //const { userInfo, setUserInfo } = useContext(UserContext);
+  const [userInfo, setUserInfo] = useRecoilState(userState);
+
+  const onclickSwitch = () => setUserInfo({ isAdmin: !userInfo.isAdmin });
+  return (
+    <SContainer>
+      <h2>ユーザー一覧</h2>
+      <SearchInput />
+      <br />
+      <SecondaryButton onClick={onclickSwitch}>切り替え</SecondaryButton>
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </SUserArea>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
+`;
---
+++ src/router/Router.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Top } from "../components/pages/Top";
+import { Users } from "../components/pages/Users";
+import { DefaultLayout } from "../components/template/DefaultLayout";
+import { HeaderOnly } from "../components/template/HeaderOnly";
+
+// Router コンポーネントの render メソッドを確認
+export const Router = () => {
+  console.log("Inside Router component"); // デバッグ用ログ
+
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <DefaultLayout>
+            <Top />
+          </DefaultLayout>
+        </Route>
+        <Route path="/users">
+          <HeaderOnly>
+            <Users />
+          </HeaderOnly>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
